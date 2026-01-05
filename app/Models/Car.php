d6789dd695ad058292893a3cae4c8b6341d97122
Andre Zarb <andre.zarb.g5341@mcast.edu.mt>
2026-01-05 17:28:26 +0000

    Initial Laravel project setup

---
+++ app/Models/Car.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Dealer;
+use App\Models\CarImage;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    public function dealer()
+    {
+        return $this->belongsTo(Dealer::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(CarImage::class);
+    }
+}
---
+++ app/Models/Dealer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Car;
+
+class Dealer extends Model
+{
+    use HasFactory;
+
+    public function cars()
+    {
+        return $this->hasMany(Car::class);
+    }
+}
